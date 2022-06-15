fedb3b521a8fdfb1540454a0459c39adcdd16b45
Anastas20 <106117530+Anastas20@users.noreply.github.com>
2022-06-15 22:03:00 +0300

    Add files via upload

--- Grebneva_ZBPI211.py
+++ Grebneva_ZBPI211.py
@@ -1,4 +1,6 @@
 # Задание 1
+from statistics import mean
+
 
 def fact(x):
     if x <= 0:
@@ -7,7 +9,7 @@ def fact(x):
         return x * fact(x - 1)
 
 
-print(fact(int(input("Введите число факториал, которого хотите узнать"))))
+# print(fact(int(input("Введите число факториал, которого хотите узнать"))))
 
 
 # Задание 2
@@ -17,10 +19,10 @@ def filter_even(li):
     list1 = list(func)
     return list1
 
-string = (input("Введите цифры через пробел")).split()
-string = [int(x) for x in string]
+# string = (input("Введите цифры через пробел")).split()
+# string = [int(x) for x in string]
 
-print(filter_even(string))
+# print(filter_even(string))
 
 
 # Задание 3
@@ -30,10 +32,7 @@ def square(li):
   return li
 
 
-lst = list(map(int, input("Введите цифры через пробел").split()))
-
-
-print(square(lst))
+# print(square(lst))
 
 
 # Задание 4
@@ -54,14 +53,14 @@ def bin_search(li, element):
   return index
 
 
-lst = list(map(int, input("Введите цифры через пробел").split()))
+# lst = list(map(int, input("Введите цифры через пробел").split()))
 # если ввели неотсортированный список и имеются повторяющиеся элементы
-lst = list(set(lst))
-lst.sort()
+# lst = list(set(lst))
+# lst.sort()
 
-el = int(input("Введите элемент, индекс которого необходимо найти"))
+# el = int(input("Введите элемент, индекс которого необходимо найти"))
 
-print(bin_search(lst, el))
+# print(bin_search(lst, el))
 
 
 # Задание 5
@@ -80,51 +79,28 @@ def is_palindrome(string):
         return "YES"
 
 
-print(is_palindrome(input("Введите строку ")))
+# print(is_palindrome(input("Введите строку ")))
 
 
 # Задание 6
 
-import re
 
 def calculate(path2file):
-    print(path2file)
-    global file_lines
-    list_values = []
-
-    for i in file_lines:
-        digits = re.findall(r"[-\s]\d{1,6}", i)
-        print(digits)
-        digit1 = int(digits[0])
-        digit2 = int(digits[1])
-        if i[0] == "+":
-            value = digit1 + digit2
-        elif i[0] == "-":
-            value = digit1 - digit2
-        elif i[0] == "%":
-            value = digit1 % digit2
-        elif i[0] ==  "*" and i[1] == "*":
-            value = digit1 ** digit2
-        elif i[0] == "*" and i[1] != "*":
-            value = digit1 * digit2
-        elif i[0] == "/" and i[1] == "/":
-            value = digit1 // digit2
-        list_values.append(value)
-
-    list_values = map(str, list_values)
-    list_values = ",".join(list_values)
-    return list_values
-
-
-
-
-file = open("6.1", "r")
-
-file_lines = file.readlines()
-file.close()
-
-with open("6.1") as file:
-    print(calculate(file.read()))
+    operations={
+        '+':lambda a,b:a+b,
+        '-':lambda a,b:a-b,
+        '*':lambda a,b:a*b,
+        '//': lambda a,b:a//b,
+        '%':lambda a,b:a%b,
+        '**':lambda a,b:a**b
+        }
+    listout=[]
+    with open(path2file, 'r') as finput:
+        for expr in finput:
+            expr = expr.strip().split('    ')
+            if expr[0] in operations:
+                listout.append(str(operations[expr[0]](int(expr[1]),int(expr[2]))))
+    return ','.join(listout)
 
 
 
@@ -133,50 +109,27 @@ with open("6.1") as file:
 import re
 
 
-def substring_slice(path2file_1, path2file_2):
-    global lst1, lst2
-    list_numbers = []
-    # преобразуем числа из файла №2
-    for i in lst2:
-        list_numbers.append(re.findall(r"\d{1,101}", i))
-    list1 = []
-    for i in list_numbers:
-        j = list(map(int, i))
-        list1.append(j)
-
-    new_string = []
-    i = 0
-    for string in lst1:
-        digit1 = list1[i][0]
-        digit2 = list1[i][1] + 1
-        s = string[digit1:digit2]
-        new_string.append(s)
-        i += 1
-
-    new_string = ' '.join(new_string)
-    return new_string
-
-
-with open("7.1") as lst1:
-    lst1 = lst1.readlines()  # список строк
-with open("7.1") as file1:
-    file1 = file1.read()
-
-with open("7.2") as lst2:
-    lst2 = lst2.readlines()  # список строк чисел
-with open("7.2") as file2:
-    file2 = file2.read()
-
-print(substring_slice(file1, file2))
+def substring_slice(path2file_1,path2file_2):
+    with open(path2file_1, 'r') as text1:
+        text1_lines = text1.readlines()
+    with open(path2file_2, 'r') as text2:
+        text2_lines = text2.readlines()
+    outlist=[]
+    t1_len = len(text1_lines)
+    for i in range(t1_len):
+        substrt2 = text2_lines[i].strip().split()
+        outlist.append(text1_lines[i][int(substrt2[0]):int(substrt2[1])+1])
+    return ' '.join(outlist)
 
 
 # Задание 8
 
-import json
-import re
+
 
 def decode_ch(sting_of_elements):
-    global periodic_table  # словарь соответствий
+    import json
+    import re
+    periodic_table = json.load(open('periodic_table.json', "r", encoding="utf-8"))
     list1 = list(periodic_table.keys())  # список названий элементов
     print(list1)
     print(sting_of_elements)
@@ -194,65 +147,40 @@ def decode_ch(sting_of_elements):
 
 
 
-periodic_table = json.load(open('periodic_table.json', "r", encoding="utf-8"))
-print(periodic_table)
-
-string = input("Введите названия элементов без разделителей")
-
-print(decode_ch(string))
-
-
 # Задание 9
 
 
 class Student:
 
-
-    def __init__(self, name, surname, *args):
+    def __init__(self, name, surname, grades=[3, 4, 5]):
         self.name = name
         self.surname = surname
-        self.fullname = name + " " + surname
-        self.grades = args
-        if self.grades == ():
-            self.grades = [3, 4, 5]
-
-
+        self.fullname = name + ' ' + surname
+        self.grades = grades
 
     def greeting(self):
-        return "Hello, I am Student"
-
+        return 'Hello, I am Student'
 
     def mean_grade(self):
-        result = sum(self.grades) / len(self.grades)
-        return result
+        return mean(self.grades)
 
     def is_otlichnik(self):
-        if self.mean_grade() >= 4.5:
-            return "YES"
-        else:
-            return "NO"
+        return 'YES' if self.mean_grade() >= 4.5 else 'NO'
 
-    def print(self):
+    def __add__(self, other):
+        return self.name + ' is friends with ' + other.name
+
+    def __str__(self):
         return self.fullname
 
 
 
-st = Student("Ivan", "Ivanov", 4, 5)
-print(st)
-print(st.__dict__)
-print(st.name)
-print(st.greeting())
-print(st.mean_grade())
-print(st.is_otlichnik())
-print(st.print())
 
-st2 = Student("Denis", "Denisov", 2, 3, 4)
-print(st2)
-print(st2.__dict__)
-print(st2.name)
-print(st2.greeting())
-print(st2.mean_grade())
-print(st2.is_otlichnik())
-print(st2.print())
+# Задание 10
+
+class MyError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        super().__init__(self.msg)
 
 
